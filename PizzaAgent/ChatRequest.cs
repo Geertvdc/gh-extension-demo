1bd52f7bc0883f7d37031411fcabae1779bd6031
Geert van der Cruijsen <geert@vdcruijsen.net>
2025-02-19 09:53:08 +0000

    removed pizza agent example

--- PizzaAgent/ChatRequest.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.AI;
-
-namespace PizzaAgent;
-
-public class ChatRequest
-{
-    public bool Stream { get; set; }
-    public List<ChatMessage> Messages { get; set; } = [];
-}
